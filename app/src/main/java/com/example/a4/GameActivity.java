9423a3c1eff262f34ec1eceba7210c5a7762b6e4
Ritayan Chakraborty <ritayanout@gmail.com>
2020-02-18 10:22:39 +0530

    Fix broken build

--- app/src/main/java/com/example/a4/GameActivity.java
+++ app/src/main/java/com/example/a4/GameActivity.java
@@ -11,8 +11,6 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
